a54deef269789af65f9a39a3624b129f853c7cc1
Taha Khan <tahashareef98@gmail.com>
2020-06-15 00:51:39 +0500

    noor.js Created

---
+++ src/compnent/noor.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Noor = () => {
+    return (
+        <div className="App">
+            My name is Noor ul Ain
+        </div>
+    )
+}
+
+export default Noor;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Noor from './compnent/noor';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Noor />
   </React.StrictMode>,
   document.getElementById('root')
 );
